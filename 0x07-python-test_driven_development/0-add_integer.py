ac0bc45a040082c3499458b8e23a4f00d28fdb9c
TanjereeN98 <ahmadcyc35@gmail.com>
2023-07-03 11:55:40 +0300

    0. Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
+"""
+0-add_integer module
+This module tests the add_integer function with mutiple cases
+The function takes two arguments, a and b, and returns their sum.
+"""
 def add_integer(a, b=98):
     
-    """
-    adds up 2 integers only
-
-    Parameters:
-    a (int): first parameter
-    b (int): second parameter
-
-    Returns:
-    int: addition of a + b
-
+    """adds up 2 integers only
+    Returns: sum of 2 integers
     """
     if not isinstance(a, (int, float)) or isinstance(a, bool):
         raise TypeError("a must be an integer")
