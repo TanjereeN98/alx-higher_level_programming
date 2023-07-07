adea0d3d98ce76917d5c23345707556d3529f6a4
TanjereeN98 <ahmadcyc35@gmail.com>
2023-07-07 13:09:28 +0300

    Fixing task 1

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -11,16 +11,14 @@ def add_integer(a, b=98):
     """adds up 2 integers only
     Returns: sum of 2 integers
     """
-    if a is None:
-        raise TypeError("add_integer() missing 1 required argument")
     if not isinstance(a, (int, float)) or isinstance(a, bool):
         raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)) or isinstance(b, bool):
+        raise TypeError("b must be an integer")
     if not (-1e308 < a < 1e308):
         raise TypeError("a must not be infinite or NaN")
     if not (-1e308 < b < 1e308):
         raise TypeError("b must not be infinite or NaN")
-    if not isinstance(b, (int, float)) or isinstance(b, bool):
-        raise TypeError("b must be an integer")
     a = int(a)
     b = int(b)
 
