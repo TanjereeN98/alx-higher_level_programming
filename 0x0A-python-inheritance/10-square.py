d40a4ad60eb4d7ad3b833ec530ffdfbb211e7758
TanjereeN98 <ahmadcyc35@gmail.com>
2023-07-11 00:08:05 +0300

    Adding some tests

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -4,16 +4,12 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """sub class Square"""
+    """class Square"""
     def __init__(self, size):
-        """Initialize the square"""
+        """init the square"""
         self.__size = size
         super().integer_validator("size", size)
 
     def area(self):
-        """Square Area"""
+        """area of the square"""
         return self.__size * self.__size
-
-    def __str__(self):
-        """print format of the square"""
-        return f"[Rectangle] {self.__size}/{self.__size}"
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-"""Rectangle Derived Class"""
-
-
+"""Rectangle class"""
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Rectangle SubClass from BaseGeometry"""
+    """Rectangle class"""
     def __init__(self, width, height):
-        """Self instantiation"""
+        """init the height and width"""
         super().integer_validator("width", width)
         self.__width = width
         super().integer_validator("height", height)
         self.__height = height
 
     def area(self):
-        """Area Method"""
-        return self.__width * self.__height
+        """the area of the rectangle"""
+        return self.__height * self.__width
 
     def __str__(self):
+        """print format of the rectangle"""
         return f"[Rectangle] {self.__width}/{self.__height}"
