80bc256519ce9664ceafa8142696a0a4a81c41d7
TanjereeN98 <ahmadcyc35@gmail.com>
2023-07-12 02:13:39 +0300

    Fixing task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """read_file function"""
 
 
 def read_file(filename=""):
-    """reading function"""
-    with open(filename, 'r', encoding="utf-8") as F:
-        print(F.read(), end='')
+    """read the file and print its content to the stdout"""
+    with open(filename, 'r', encoding="utf-8") as f:
+        print(f.read(), end='')
