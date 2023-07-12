16cc8dbf6f2150231fc36de2316fdbcb39f91f1a
TanjereeN98 <ahmadcyc35@gmail.com>
2023-07-12 03:09:59 +0300

    Fixing task 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -8,12 +8,12 @@ def pascal_triangle(n):
     if n <= 0:
         return []
     tris = [[1]]
-    while n != len(tris):
+    while len(tris) != n:
         tri = tris[-1]
         tmp = [1]
         for i in range(len(tri) - 1):
             tmp.append(tri[i] + tri[i + 1])
         tmp.append(1)
-        trs.append(tmp)
+        tris.append(tmp)
     return tris
 
