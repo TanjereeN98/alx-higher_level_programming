dc99662ae76ae9fa64ae4a7bb8c8933df9ccff12
TanjereeN98 <ahmadcyc35@gmail.com>
2023-07-12 03:08:53 +0300

    Adding task 12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""pascal_triangle function"""
+
+
+def pascal_triangle(n):
+    """returns a list of lists of integers representing
+    the Pascal’s triangle of n"""
+    if n <= 0:
+        return []
+    tris = [[1]]
+    while n != len(tris):
+        tri = tris[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        trs.append(tmp)
+    return tris
+
