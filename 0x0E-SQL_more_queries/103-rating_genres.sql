6902efcf9eda94666d557b90a98e7c1e74f7d816
TanjereeN98 <ahmadcyc35@gmail.com>
2023-08-17 04:13:48 +0300

    Adding 4th advanced task

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -2,6 +2,7 @@
 SELECT tg.name AS name, SUM(tsr.rating) AS rating
 FROM tv_genres AS tg
 JOIN tv_show_genres AS tsg ON tsg.genre_id = tg.id
-JOIN tv_show_ratings AS tsr ON tsr.show_id = tsg.show_id
+JOIN tv_shows AS ts ON ts.id = tsg.show_id
+JOIN tv_show_ratings AS tsr ON tsr.show_id = ts.id
 GROUP BY tg.name
 ORDER BY rating DESC;
