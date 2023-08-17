130929e4f27ffe0e38cf8882f89e1a3892ed75dc
TanjereeN98 <ahmadcyc35@gmail.com>
2023-08-17 04:12:26 +0300

    Adding 4th advanced task

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,7 +1,7 @@
 -- a script that lists all genres in the database hbtn_0d_tvshows_rate by their rating.
 SELECT tg.name AS name, SUM(tsr.rating) AS rating
 FROM tv_genres AS tg
-JOIN tv_show_genres as tsg ON tsg.genre_id = tg.id
+JOIN tv_show_genres AS tsg ON tsg.genre_id = tg.id
 JOIN tv_show_ratings AS tsr ON tsr.show_id = tsg.show_id
 GROUP BY tg.name
 ORDER BY rating DESC;
