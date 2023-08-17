90b80ac87f0de4a1a558ed47777eb495d35f677a
TanjereeN98 <ahmadcyc35@gmail.com>
2023-08-17 03:59:29 +0300

    adding 3rd advanced task

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- script that lists all shows from hbtn_0d_tvshows_rate by their rating.
-SELECT ts.title AS title, SUM(tsr.rate)
+SELECT ts.title AS title, SUM(tsr.rate) AS rating
 FROM tv_shows AS ts
 JOIN tv_show_ratings AS tsr ON ts.id = tsr.show_id
 GROUP BY ts.title
-ORDER BY SUM(tsr.rate) DESC;
+ORDER BY rating DESC;
