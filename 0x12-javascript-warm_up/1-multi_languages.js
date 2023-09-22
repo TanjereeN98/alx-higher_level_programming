bdec7c5158fc9dd2fe709bab508d20c4e4ce31f4
TanjereeN <96239912+TanjereeN98@users.noreply.github.com>
2023-09-22 23:05:16 +0300

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/1-multi_languages.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/node
-console.log(`C is fun
-Python is cool
-JavaScript is amazing`);
--- 0x12-javascript-warm_up/11-second_biggest.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-if (process.argv.length === 2 || process.argv.length === 3) { console.log(0); } else {
-  process.argv.sort((a, b) => b - a);
-  console.log(process.argv[3]);
-}
