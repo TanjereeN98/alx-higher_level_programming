fda6fa5277a0d83e48d35b365833da26fa57f8ba
TanjereeN98 <ahmadcyc35@gmail.com>
2024-02-07 13:45:57 +0200

    adding updates

--- air/models/base_model.py
+++ air/models/base_model.py
@@ -19,7 +19,6 @@ class BaseModel:
         
         self.id = str(uuid.uuid4())
         self.created_at = self.updated_at = datetime.now()
-        print('Where')
         models.storage.new(self)
         
     def __str__(self):
