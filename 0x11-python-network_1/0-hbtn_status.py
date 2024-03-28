360be629c79acbd8c32a39c3b888f9809c84d134
TanjereeN98 <ahmadcyc35@gmail.com>
2024-03-28 23:15:26 +0200

    Adding task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,10 +5,10 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    req = "https://alx-intranet.hbtn.io/status"
     with urllib.request.urlopen(req) as response:
         data = response.read()
         utf_data = data.decode('utf-8')
         resType = type(data)
-        print(f"Body respose:\n\t- type: {resType}\n\t- \
+        print(f"Body response:\n\t- type: {resType}\n\t- \
 content: {data}\n\t- utf8 content: {utf_data}")
