f304f01ec5b9f8867e31a4b0b33337d92bdafcf2
TanjereeN98 <ahmadcyc35@gmail.com>
2024-03-29 01:18:06 +0200

    Correcting task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     payload = {'email': email}
-    r = requests.get(url, params=payload)
+    r = requests.get(url, data=payload)
     print(r.text)
