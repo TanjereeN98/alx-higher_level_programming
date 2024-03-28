99a210d03d253d88e65e576e91a07fa326c1ca02
TanjereeN98 <ahmadcyc35@gmail.com>
2024-03-28 04:19:25 +0200

    Adding check if the pointer is null

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,15 +4,16 @@
 
 def find_peak(list_of_integers):
     """a function that finds a peak in a list of unsorted integers."""
-    if len(list_of_integers) == 1:
-        return list_of_integers[0]
-    for i in range(len(list_of_integers)):
-        try:
-            if list_of_integers[i] >= list_of_integers[i-1]:
-                if list_of_integers[i] >= list_of_integers[i+1]:
-                    return list_of_integers[i]
-            else:
+    if list_of_integers:
+        if len(list_of_integers) == 1:
+            return list_of_integers[0]
+        for i in range(len(list_of_integers)):
+            try:
+                if list_of_integers[i] >= list_of_integers[i-1]:
+                    if list_of_integers[i] >= list_of_integers[i+1]:
+                        return list_of_integers[i]
+                else:
+                    continue
+            except IndexError:
                 continue
-        except IndexError:
-            continue
-    return None
+        return None
