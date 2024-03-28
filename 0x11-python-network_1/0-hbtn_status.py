e0675038c30a7d615138b0e7ea8675dffe02b9ca
TanjereeN98 <ahmadcyc35@gmail.com>
2024-03-28 23:13:45 +0200

    Adding task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""0. What's my status? #0"""
+
+import urllib.request
+
+
+if __name__ == "__main__":
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        data = response.read()
+        utf_data = data.decode('utf-8')
+        resType = type(data)
+        print(f"Body respose:\n\t- type: {resType}\n\t- \
+content: {data}\n\t- utf8 content: {utf_data}")
