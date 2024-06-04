4067c4c8b9270c4780aa9b8a29502ca4b6db2ec7
TanjereeN98 <ahmadcyc35@gmail.com>
2024-06-05 01:33:18 +0300

    Task 3 - Star wars movie title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const movieID = process.argv[2];
+const request = require('request');
+
+request(`https://swapi-api.alx-tools.com/api/films/${movieID}`, function (error, response, body) {
+  if (error) {
+    console.error('error:', error && error);
+  }
+  console.log(JSON.parse(body).title);
+});
