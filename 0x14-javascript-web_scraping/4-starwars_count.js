4875ea43431fc1ad8931db115911b1d7dae6f33f
TanjereeN98 <ahmadcyc35@gmail.com>
2024-06-05 02:06:13 +0300

    Task 4 - Star wars Wedge Antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const apiUrl = process.argv[2];
+const request = require('request');
+
+request(`${apiUrl}`, function (error, response, body) {
+  if (error) {
+    console.error('error:', error && error);
+  }
+  const myData = JSON.parse(body).results;
+  const data = myData.filter(f => f.characters.find(c => c.includes('18')));
+  console.log(data.length);
+});
