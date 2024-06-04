a41688ed28d5348cb31781ded5ca5c0382f998fc
TanjereeN98 <ahmadcyc35@gmail.com>
2024-06-05 01:09:43 +0300

    Task 2 - Status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const urlRequest = process.argv[2];
+const request = require('request');
+
+request(urlRequest, function (error, response, body) {
+  if (error) {
+    console.error('error:', error && error);
+  }
+  console.log('code:', response && response.statusCode);
+});
