8303eb840054a784c35d79a6c83d44189ff9e75e
TanjereeN98 <ahmadcyc35@gmail.com>
2024-06-05 02:13:11 +0300

    Task 5 - Loripsum

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const requestedUrl = process.argv[2];
+const filePath = process.argv[3];
+const request = require('request');
+const fs = require('node:fs');
+
+request(`${requestedUrl}`, function (error, response, body) {
+  if (error) {
+    console.error('error:', error && error);
+  }
+  fs.writeFile(filePath, body, 'utf8', err => {
+    if (err) {
+      console.error(err);
+    }
+  });
+});
